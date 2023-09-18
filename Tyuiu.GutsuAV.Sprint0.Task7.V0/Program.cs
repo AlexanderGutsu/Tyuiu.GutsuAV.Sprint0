87468e4bdba9c50c935c0ea3a1efe711e2ba3923
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-09-18 23:59:41 +0300

    Task7

--- Tyuiu.GutsuAV.Sprint0.Task7.V0/Program.cs
+++ Tyuiu.GutsuAV.Sprint0.Task7.V0/Program.cs
@@ -27,6 +27,10 @@ namespace Tyuiu.GutsuAV.Sprint0.Task7.V0
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
 
             int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
             Console.WriteLine("Значение элементов массива №1");
@@ -43,10 +47,6 @@ namespace Tyuiu.GutsuAV.Sprint0.Task7.V0
                 Console.WriteLine(arrayNums2[i] + " , ");
             }
 
-            Console.WriteLine();
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
-            Console.WriteLine("***************************************************************************");
             if (arrayNums1.Length == arrayNums2.Length)
             {
                 int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
